907e3390ec117feaf6666c0a58647f3520a83d0f
Sergei Zaikin <sergeizaikin@hotmail.com>
2019-11-10 12:29:41 -0300

    Optimization of the part of adding contract in Program and changing the method of date parsing to show it in output

--- Program.cs
+++ Program.cs
@@ -10,53 +10,64 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter Department's name: ");
+            //Department data
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("Enter department's name: ");
             string dptName = Console.ReadLine();
             Department department = new Department(dptName);
 
-            Console.WriteLine("Enter worker data: ");
+            //Worker's data
+            Console.WriteLine("Enter worker's data: ");
+            Console.ResetColor();
+
             Console.Write("Name: ");
             string name = Console.ReadLine();
             Console.Write("Level (Junior/MidLevel/Senior): ");
             WorkerLevel level = Enum.Parse<WorkerLevel>(Console.ReadLine());
             Console.Write("Base salary: ");
             double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
             Worker worker1 = new Worker(name, level, salary, department);
+            Console.WriteLine();
 
+            //Contracts data
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.Write("How many contracts this worker has?: ");
+            Console.ResetColor();
             int qtdContracts = int.Parse(Console.ReadLine());
             List<HourContract> contracts = new List<HourContract>();
 
             for (int i = 0; i < qtdContracts; i++)
             {
+                Console.ForegroundColor = ConsoleColor.Yellow;
                 Console.WriteLine($"Enter contract #{i+1} data:");
+                Console.ResetColor();
                 Console.Write("Date (DD/MM/YYYY): ");
                 DateTime date = DateTime.Parse(Console.ReadLine());
                 Console.Write("Value per hour: ");
                 double valPerHour = double.Parse(Console.ReadLine());
                 Console.Write("Duration (hours): ");
                 int duration = int.Parse(Console.ReadLine());
-
-                contracts.Add(new HourContract(date, valPerHour, duration));
+                Console.WriteLine();
+                worker1.AddContract(new HourContract(date, valPerHour, duration));
+                
             }
 
-            foreach (var contract in contracts)
-            {
-                worker1.AddContract(contract);
-            }
-
+            //Test output
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.Write("Enter month and year to calculate income (MM/YYYY): ");
-            string[] dateIncome = Console.ReadLine().Split('/');
-            int month = int.Parse(dateIncome[0]);
-            int year = int.Parse(dateIncome[1]);
+            Console.ResetColor();
+
+            //string[] dateIncome = Console.ReadLine().Split('/');
+            //int month = int.Parse(dateIncome[0]);
+            //int year = int.Parse(dateIncome[1]);
+
+            string dateIncome = Console.ReadLine();
+            int month = int.Parse(dateIncome.Substring(0, 2));
+            int year = int.Parse(dateIncome.Substring(3, 4));
 
             Console.WriteLine($"Name: {worker1.Name}");
             Console.WriteLine($"Department: {worker1.Department.Name}");
-            Console.WriteLine($"Income: {worker1.Income(year,month)}");
-
-
-
+            Console.WriteLine($"Income for {dateIncome}: {worker1.Income(year,month)}");
 
         }
 
